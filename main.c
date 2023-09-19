0bacd9ba5178202400ae14d1b4773f278c63760f
root <aminoxlhd>
2023-09-19 16:08:21 -0700

    edit main.c and main.h

--- main.c
+++ main.c
@@ -1,63 +1,97 @@
 #include "main.h"
 
-int main(int argc, char *argv[])
+/**
+ * main - Entry Point.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	size_t bufsize = 1024;
 	char *binaryPath;
 	char **args;
-	pid_t pid;
-	int i = 0;
+	int i = 0, status = 0;
 
 	while (1)
 	{
 		printf("#cisfun$ ");
-		binaryPath = (char *) malloc(bufsize * sizeof(char))
+		binaryPath = (char *) malloc(bufsize * sizeof(char));
 		getline(&binaryPath, &bufsize, stdin);
 		remove_last_newline(binaryPath);
-		agrs = split_arguments(binaryPath, " ");
-
+		args = split_arguments(binaryPath, " ");
 		if (check_command(args[0]) == 1)
 		{
-		if (strcmp(args[0], "exit") == 0)
-		{
-		break;
-		}
-		if (strcmp(args[0], "env") == 0)
-		{
-		i = 0;
-		while (environ[i])
-		{
-		printf("%s\n", environ[i]);
-		i++;
-		}
-		continue;
-		}
-
-		pid = fork();
-		if (pid == 0)
-		{
-			int exec = run_shell(agrs);
-
-			if (exec == -1)
+			if (strcmp(args[0], "exit") == 0)
 			{
-				print_error(agrs[0]);
+				status = handle_exit(args);
+				break;
 			}
-			exit(1);
-		}
-		else
-		{
-			wait(NULL);
-		}
-		}
-		else
+			if (strcmp(args[0], "env") == 0)
+			{
+				i = 0;
+				while (environ[i])
+				{
+					printf("%s\n", environ[i]);
+					i++;
+				}
+				continue;
+			}
+			run_command(args);
+		} else
 		{
 			print_error(args[0]);
 		}
 		free(binaryPath);
 	}
-	return (0);
+	return (status);
 }
 
+/**
+ * handle_exit - a function that will be called
+ * when we type exit in the command line
+ * @args : the command line we typed.
+ * Return: 0 if there is no status
+ * status if we add a status to exit (e.g exit 3)
+ */
+int handle_exit(char **args)
+{
+	int sizeOfArgs = 0, i = 0, status = 0;
+
+	for (i = 0; args[i] != NULL; i++)
+		sizeOfArgs++;
+	if (sizeOfArgs == 2)
+		status = atoi(args[1]);
+	return (status);
+}
+
+/**
+ * run_command - a function that will create a fork
+ * and run the command, if the command doesn't exists
+ * it'll print an error
+ * @args : tokenized command with arguments.
+ */
+void run_command(char **args)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		int exec = run_shell(args);
+
+		if (exec == -1)
+			print_error(args[0]);
+		exit(1);
+	} else
+	{
+		wait(NULL);
+	}
+}
+
+/**
+ * print_error - Function that prints the error if a command isn't found.
+ * @command : a string which is the command.
+ */
 void print_error(char *command)
 {
 	char *shell = getenv("SHELL");
--- main.h
+++ main.h
@@ -9,23 +9,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 extern char **environ;
-int run_shell(char ** command);
-char ** split_arguments(char *str, char * delim);
-char * get_path(char * command);
-int check_command(char * command);
-void print_env();
-void remove_last_newline(char * string);
-void custom_getline(char * string);
-void print_error(char * command);
-
-typedef struct Alias {
-    char *name;
-    char *value;
-} Alias;
-
-void manage_aliases(void);
-void add_alias(char *name, char *value);
-void print_aliases();
-void remove_alias(const char *name)
-
+int run_shell(char **command);
+char **split_arguments(char *str, char *delim);
+char *get_path(char *command);
+int check_command(char *command);
+void print_env(void);
+void remove_last_newline(char *string);
+void custom_getline(char *string);
+void run_command(char **args);
+void print_error(char *command);
+char *m_strtok(char* string, const char* delim);
+int handle_exit(char **args);
 #endif
