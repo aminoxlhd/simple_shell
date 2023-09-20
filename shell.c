a1488570ad42538cc37549f9e22a98fd266cf4fd
root <aminoxlhd>
2023-09-19 21:11:19 -0700

    edit shell.c

--- shell.c
+++ shell.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
-* run_shell - Function that run a shell command.
-* the funciton will try to run the simple command, if it's not found, it'll look in the PATH.
-* if not found will return -1.
-*/
-int run_shell(char ** command)
+ * run_shell - Function that run a shell command
+ * @command: the funciton will try to run the simple command
+ * Return: if not found will return -1
+ */
+int run_shell(char **command)
 {
 	char *environments[] = { NULL };
 	int execution = 0;
 
-	execution = execve(command[0] , command, environments);
+	execution = execve(command[0], command, environments);
 
 	if (execution == -1)
 	{
-		char * command_with_path = get_path(command[0]);
+		char *command_with_path = get_path(command[0]);
 
 		if (command_with_path == NULL)
 		{
 			return (-1);
 		}
-		execution = execve(command_with_path, command, environments)
+		execution = execve(command_with_path, command, environments);
 	}
 
 	return (execution);
